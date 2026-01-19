ea39b800a723d28292602d66d623d6c3e87a7f06
lijuncai <516346556@qq.com>
2026-01-19 23:35:45 +1100

    📝using latest dayflow version

--- website/postcss.config.mjs
+++ website/postcss.config.mjs
@@ -1,8 +1,5 @@
 const config = {
-  plugins: [
-    "@tailwindcss/postcss",
-    ["cssnano", { preset: ["default", { mergeRules: false }] }]
-  ],
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
