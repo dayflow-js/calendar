0543a31e76ef5e1f88e52eecff12c3ea9c1c317c
lijuncai <516346556@qq.com>
2026-02-22 09:40:24 +1100

    docs: update calendarTypes and fix image issue

--- website/mdx-components.tsx
+++ website/mdx-components.tsx
@@ -1,12 +1,16 @@
 import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs';
 import { FrameworkInstall } from './components/FrameworkInstall';
 import { FrameworkTabs } from './components/FrameworkTabs';
+import { withBasePath } from './utils/basePath';
 
 const docsComponents = getDocsMDXComponents();
 
-export function useMDXComponents(components) {
+export function useMDXComponents(components: any): any {
   return {
     ...docsComponents,
+    img: (props: any) => (
+      <img {...props} src={withBasePath(props.src)} />
+    ),
     FrameworkInstall,
     FrameworkTabs,
     ...components,
