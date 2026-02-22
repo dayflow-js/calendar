9370e746b788344af815ca7864604a5921a9bd7d
lijuncai <516346556@qq.com>
2026-02-23 07:46:43 +1100

    fix: website image missing

--- website/mdx-components.js
+++ website/mdx-components.js
@@ -5,12 +5,20 @@ import { withBasePath } from './utils/basePath';
 
 const docsComponents = getDocsMDXComponents();
 
+// DocImg is used instead of <img> in MDX files because explicit JSX <img> elements
+// bypass the useMDXComponents override (only markdown ![]() goes through it).
+// Using an uppercase component name ensures MDX routes it through the components system.
+function DocImg(props) {
+  return <img {...props} src={withBasePath(props.src)} />;
+}
+
 export function useMDXComponents(components) {
   return {
     ...docsComponents,
     img: (props) => (
       <img {...props} src={withBasePath(props.src)} />
     ),
+    DocImg,
     FrameworkInstall,
     FrameworkTabs,
     ...components,
